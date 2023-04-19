7a94a6243177be9a84fdf5e33961b21076022b0b
yykzjh <1378453948@qq.com>
2023-04-19 15:32:28 +0800

    add trainer and tester

---
+++ my-code/lib/testers/tester.py
@@ -0,0 +1,243 @@
+import os
+import math
+import cc3d
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import numpy as np
+from functools import reduce
+from nibabel.viewers import OrthoSlicer3D
+
+import configs.config as config
+
+from lib import utils
+from lib.visualizations.AverageMeterWriter import AverageMeterWriter
+from lib.visualizations.SegmentMapDisplayer import display_compare_hist, display_segmentation_3D
+
+
+
+class Tester():
+    """
+    Tester class
+    """
+    def __init__(self, model, meter, device):
+        """
+        Args:
+            model: 网络模型
+            meter: 评价指标度量器
+            device: 设备
+        """
+        # 传入的参数
+        self.model = model
+        self.meter = meter
+        self.device = device
+
+        # 初始化指标统计打印工具
+        self.writer = AverageMeterWriter()
+
+
+    def split_test(self, image):
+        # 获取图像尺寸
+        ori_shape = image.size()[2:]
+        # 初始化输出的特征图
+        output = torch.zeros((image.size()[0], config.classes, *ori_shape), device=self.device)
+        # 切片的大小
+        slice_shape = config.crop_size
+        # 在三个维度上滑动的步长
+        stride = config.crop_stride
+        # 计算总共要预测多少张切片
+        total_slice_num = 1
+        for i in range(3):
+            total_slice_num *= math.ceil((ori_shape[i] - slice_shape[i]) / stride[i]) + 1
+
+        # 设置进度条
+        with tqdm(desc="滑动切片分割", leave=True, total=total_slice_num, unit="slice", ncols=200, ascii=True) as bar:
+            # 在三个维度上进行滑动切片
+            for shape0_start in range(0, ori_shape[0], stride[0]):
+                shape0_end = shape0_start + slice_shape[0]
+                start0 = shape0_start
+                end0 = shape0_end
+                if shape0_end >= ori_shape[0]:
+                    end0 = ori_shape[0]
+                    start0 = end0 - slice_shape[0]
+
+                for shape1_start in range(0, ori_shape[1], stride[1]):
+                    shape1_end = shape1_start + slice_shape[1]
+                    start1 = shape1_start
+                    end1 = shape1_end
+                    if shape1_end >= ori_shape[1]:
+                        end1 = ori_shape[1]
+                        start1 = end1 - slice_shape[1]
+
+                    for shape2_start in range(0, ori_shape[2], stride[2]):
+                        shape2_end = shape2_start + slice_shape[2]
+                        start2 = shape2_start
+                        end2 = shape2_end
+                        if shape2_end >= ori_shape[2]:
+                            end2 = ori_shape[2]
+                            start2 = end2 - slice_shape[2]
+
+                        slice_tensor = image[:, :, start0:end0, start1:end1, start2:end2]
+                        slice_predict = self.model(slice_tensor.to(self.device))
+                        # slice_predict = nn.Softmax(dim=1)(slice_predict)
+                        output[:, :, start0:end0, start1:end1, start2:end2] += slice_predict
+                        # 进度加1
+                        bar.update(1)
+
+                        if shape2_end >= ori_shape[2]:
+                            break
+
+                    if shape1_end >= ori_shape[1]:
+                        break
+
+                if shape0_end >= ori_shape[0]:
+                    break
+
+        return output
+
+
+
+    def test_single_image_without_label(self, image):
+        self.model.eval()
+
+        with torch.no_grad():
+            # 维度扩张
+            image = torch.FloatTensor(image.numpy()).unsqueeze(0).unsqueeze(0)
+            # 将图像放到GPU上
+            image = image.to(self.device)
+            # 输入到网络
+            output = self.split_test(image)
+            # 对各通道进行softmax
+            probability_map = nn.Softmax(dim=1)(output)
+            # 根据各通道的概率，选择最大值所在的索引作为该处的类别
+            class_map = torch.argmax(probability_map, dim=1).squeeze(0).cpu().numpy()
+            # 显示
+            OrthoSlicer3D(class_map).show()
+
+
+    def test_single_image(self, image, label):
+        self.writer.reset()
+        self.model.eval()
+
+        with torch.no_grad():
+            # 维度扩张
+            image = torch.FloatTensor(image.numpy()).unsqueeze(0).unsqueeze(0)
+            label = torch.FloatTensor(label.numpy()).unsqueeze(0)
+            # 将图像放到GPU上
+            image = image.to(self.device)
+            label = label.to(self.device)
+            # 输入到网络
+            output = self.split_test(image)
+            # 计算各通道的dsc
+            per_channel_dice = self.meter.get_dice(output, label)
+            # 统计并打印
+            self.writer.update(per_channel_dice)
+            self.writer.display_terminal()
+            # 对各通道进行softmax
+            probability_map = nn.Softmax(dim=1)(output)
+            # 将训练样本中没有的类别概率置为0
+            probability_map[:, [26, 34], ...] = 0
+            # 根据各通道的概率，选择最大值所在的索引作为该处的类别
+            class_map = torch.argmax(probability_map, dim=1).squeeze(0).cpu().numpy()
+            # 转换格式
+            label_np = label.squeeze(0).cpu().numpy()
+
+            # # 输出概率图
+            # print(output[0, :, class_map == 26].permute(1, 0)[:20, :])
+            # print(output[0, :, class_map == 26].permute(1, 0)[:20, 26].reshape((-1, 1)))
+
+            # 对预测分割图进行最大连通区域处理
+            class_map = maximum_connected_component_analysis(class_map)
+
+            # 显示分布对比图
+            display_compare_hist(label_np, class_map)
+
+            # 分割图3D可视化
+            display_segmentation_3D(class_map)
+
+            OrthoSlicer3D(label_np).show()
+            OrthoSlicer3D(class_map).show()
+
+
+    def test_image_set(self, test_loader):
+        self.writer.reset()
+        self.model.eval()
+
+        for image, label in tqdm(test_loader):
+            with torch.no_grad():
+                # 将图像放到GPU上
+                image = image.to(self.device)
+                label = label.to(self.device)
+                # 输入到网络
+                output = self.split_test(image)
+                # 计算各通道的dsc
+                per_channel_dice = self.meter.get_dice(output, label)
+                # 统计并打印
+                self.writer.update(per_channel_dice)
+        self.writer.display_terminal()
+
+
+
+# 筛选每个类别的最大连通区域
+def maximum_connected_component_analysis(class_map):
+    # 每个类别只保留最大的连通区域，定义列表表示每个类别是否已经处理过了
+    rec_label = np.zeros((config.classes, ))
+    # 计算总元素个数
+    element_num = reduce(lambda x, y: x * y, class_map.shape)
+    # 拷贝一份原始图像
+    segment_map = class_map.copy()
+
+    # 定义连通性强度
+    connectivity = 26
+    # 进行连通区域分析标记，返回标记三维数组
+    labels_out, N = cc3d.connected_components(segment_map, return_N=True, connectivity=connectivity)
+    # 进一步获取连通区域的统计信息
+    """
+    {
+      voxel_counts: np.ndarray[uint64_t] (index is label) (N+1)
+
+      # Structure is xmin,xmax,ymin,ymax,zmin,zmax by label
+      bounding_boxes: np.ndarray[uint64_t] (N+1 x 6)
+
+      # Structure is x,y,z
+      centroids: np.ndarray[float64] (N+1,3)
+    }
+    """
+    statistics_data = cc3d.statistics(labels_out)
+    # 得到每个连通区域的大小，索引表示连通区域的编号，元素值为该连通区域大小
+    voxel_counts = statistics_data["voxel_counts"]
+    # 构造所有连通区域面积和label构成的元组的列表
+    components = [(voxel_counts[i], i) for i in range(1, N+1)]
+    # 按照连通区域面积从大到小排序
+    components = sorted(components, key=lambda x: x[0], reverse=True)
+
+    # 遍历所有的连通区域
+    for i in range(N):
+        # 获取当前连通区域的label
+        ccl_label = components[i][1]
+        # 获取当前连通区域的原数据值
+        ori_label = class_map[labels_out == ccl_label][0]
+        # 判断当前得到的原数值代表的牙齿类别是否已经处理过
+        if (ori_label == 1 and components[i][0] < 0.0001 * element_num) or (ori_label != 1 and rec_label[ori_label] == 1):
+            # 该连通区域不是该牙齿类别的最大连通区域, 设置为背景0
+            segment_map[labels_out == ccl_label] = 0
+        elif ori_label != 1:
+            # 该连通区域是该牙齿类别的最大连通区域
+            rec_label[ori_label] = 1
+    return segment_map
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- my-code/train.py
+++ my-code/train.py
@@ -7,22 +7,183 @@
 @License  :   (C)Copyright 2022
 """
 import os
+import glob
+import math
+import tqdm
+import shutil
+import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
+from nibabel.viewers import OrthoSlicer3D
+
 import torch
-import argparse
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+import nni
+from nni.experiment import Experiment
+
+from lib import utils, transforms, models, losses
+
+
+
+# 默认参数,这里的参数在后面添加到模型中，以params['dropout_rate']等替换原来的参数
+params = {
+    # ——————————————————————————————————————————————     启动初始化    ———————————————————————————————————————————————————
+
+    "CUDA_VISIBLE_DEVICES": "0",  # 选择可用的GPU编号
+
+    "seed": 1777777,  # 随机种子
+
+    "cuda": True,  # 是否使用GPU
+
+    "benchmark": False,  # 为整个网络的每个卷积层搜索最适合它的卷积实现算法，进而实现网络的加速。用于网络输入维度和类型不会变的情况。
+
+    "deterministic": True,  # 固定cuda的随机数种子，每次返回的卷积算法将是确定的。用于复现模型结果
+
+    # —————————————————————————————————————————————     预处理       ————————————————————————————————————————————————————
+
+    "resample_spacing": [0.5, 0.5, 0.5],  # 重采样的体素间距。三个维度的值一般相等，可设为0.5(图像尺寸有[200,200,100]、[200,200,200]、
+    # [160,160,160]),或者设为0.25(图像尺寸有[400,400,200]、[400,400,400]、[320,320,320])
+
+    "clip_lower_bound": -3566,  # clip的下边界数值
+    "clip_upper_bound": 14913,  # clip的上边界数值
+
+    "samples_train": 2046,  # 作为实际的训练集采样的子卷数量，也就是在原训练集上随机裁剪的子图像数量
+
+    "crop_size": (160, 160, 96),  # 随机裁剪的尺寸。1、每个维度都是32的倍数这样在下采样时不会报错;2、11G的显存最大尺寸不能超过(192,192,160);
+    # 3、要依据上面设置的"resample_spacing",在每个维度随机裁剪的尺寸不能超过图像重采样后的尺寸;
+
+    "crop_threshold": 0.1,  # 随机裁剪时需要满足的条件，不满足则重新随机裁剪的位置。条件表示的是裁剪区域中的前景占原图总前景的最小比例
+
+    # ——————————————————————————————————————————————    数据增强    ——————————————————————————————————————————————————————
+
+    "augmentation_probability": 0.3,  # 每张图像做数据增强的概率
+    "augmentation_method": "Choice",  # 数据增强的方式，可选["Compose", "Choice"]
+
+    # 弹性形变参数
+    "open_elastic_transform": True,  # 是否开启弹性形变数据增强
+    "elastic_transform_sigma": 20,  # 高斯滤波的σ,值越大，弹性形变越平滑
+    "elastic_transform_alpha": 1,  # 形变的幅度，值越大，弹性形变越剧烈
+
+    # 高斯噪声参数
+    "open_gaussian_noise": True,  # 是否开启添加高斯噪声数据增强
+    "gaussian_noise_mean": 0,  # 高斯噪声分布的均值
+    "gaussian_noise_std": 0.01,  # 高斯噪声分布的标准差,值越大噪声越强
+
+    # 随机翻转参数
+    "open_random_flip": True,  # 是否开启随机翻转数据增强
+
+    # 随机缩放参数
+    "open_random_rescale": True,  # 是否开启随机缩放数据增强
+    "random_rescale_min_percentage": 0.5,  # 随机缩放时,最小的缩小比例
+    "random_rescale_max_percentage": 1.5,  # 随机缩放时,最大的放大比例
+
+    # 随机旋转参数
+    "open_random_rotate": True,  # 是否开启随机旋转数据增强
+    "random_rotate_min_angle": -50,  # 随机旋转时,反方向旋转的最大角度
+    "random_rotate_max_angle": 50,  # 随机旋转时,正方向旋转的最大角度
+
+    # 随机位移参数
+    "open_random_shift": True,  # 是否开启随机位移数据增强
+    "random_shift_max_percentage": 0.3,  # 在图像的三个维度(D,H,W)都进行随机位移，位移量的范围为(-0.3×(D、H、W),0.3×(D、H、W))
+
+    # 标准化均值
+    "normalize_mean": 0.17375025153160095,
+    "normalize_std": 0.053983770310878754,
+
+    # —————————————————————————————————————————————    数据读取     ——————————————————————————————————————————————————————
+
+    "dataset_name": "3DTooth",  # 数据集名称， 可选["3DTooth", ]
+
+    "dataset_path": r"./datasets/src_10",  # 数据集路径
+
+    "create_data": True,  # 是否重新分割子卷训练集
+
+    "batch_size": 6,  # batch_size大小
+
+    "num_workers": 4,  # num_workers大小
+
+    # —————————————————————————————————————————————    网络模型     ——————————————————————————————————————————————————————
+
+    "model_name": "DENSEVNET",  # 模型名称，可选["DENSEVNET","VNET"]
+
+    "in_channels": 1,  # 模型最开始输入的通道数,即模态数
+
+    "classes": 35,  # 模型最后输出的通道数,即类别总数
+
+    "resume": None,  # 是否重启之前某个训练节点，继续训练;如果需要则指定checkpoint文件路径
+
+    "pretrain": None,  # 是否需要加载预训练权重，如果需要则指定预训练权重文件路径
+
+    # ——————————————————————————————————————————————    优化器     ——————————————————————————————————————————————————————
+
+    "optimizer_name": "Adam",  # 优化器名称，可选["SGD", "Adagrad", "RMSprop", "Adam", "Adamax", "Adadelta"]
+
+    "learning_rate": 0.001,  # 学习率
+
+    "weight_decay": 0.00001,  # 权重衰减系数,即更新网络参数时的L2正则化项的系数
+
+    "momentum": 0.9,  # 动量大小
+
+    # ———————————————————————————————————————————    学习率调度器     —————————————————————————————————————————————————————
+
+    "lr_scheduler_name": "ReduceLROnPlateau",  # 学习率调度器名称，可选["ExponentialLR", "StepLR", "MultiStepLR",
+    # "CosineAnnealingLR", "CosineAnnealingWarmRestarts", "OneCycleLR", "ReduceLROnPlateau"]
+
+    "gamma": 0.9,  # 学习率衰减系数
+
+    "step_size": 5,  # StepLR的学习率衰减步长
+
+    "milestones": [3, 5, 9, 13, 15, 17, 18, 19],  # MultiStepLR的学习率衰减节点列表
+
+    "T_max": 5,  # CosineAnnealingLR的半周期
+
+    "T_0": 4,  # CosineAnnealingWarmRestarts的周期
+
+    "T_mult": 2,  # CosineAnnealingWarmRestarts的周期放大倍数
+
+    "mode": "min",  # ReduceLROnPlateau的衡量指标变化方向
+
+    "patience": 5,  # ReduceLROnPlateau的衡量指标可以停止优化的最长epoch
+
+    "factor": 0.1,  # ReduceLROnPlateau的衰减系数
+
+    # ————————————————————————————————————————————    损失函数     ———————————————————————————————————————————————————————
+
+    "loss_function_name": "DiceLoss",  # 损失函数名称，可选["DiceLoss","CrossEntropyLoss","WeightedCrossEntropyLoss",
+    # "MSELoss","SmoothL1Loss","L1Loss","WeightedSmoothL1Loss","BCEDiceLoss","BCEWithLogitsLoss"]
+
+    "class_weight": [0.00002066, 0.00022885, 0.10644896, 0.02451709, 0.03155127, 0.02142642, 0.02350483, 0.02480525,
+                     0.01125384, 0.01206108, 0.07426875, 0.02583742, 0.03059388, 0.02485595, 0.02466779, 0.02529981,
+                     0.01197175, 0.01272877, 0.16020726, 0.05647514, 0.0285633, 0.01808694, 0.02124704, 0.02175892,
+                     0.01802092, 0.01563035, 0., 0.0555509, 0.02747846, 0.01756969, 0.02183707, 0.01934677, 0.01848419,
+                     0.01370064, 0.],  # 各类别计算损失值的加权权重
+
+    "sigmoid_normalization": False,  # 对网络输出的各通道进行归一化的方式,True是对各元素进行sigmoid,False是对所有通道进行softmax
+
+    # —————————————————————————————————————————————   训练相关参数   ——————————————————————————————————————————————————————
+
+    "run_dir": r"./runs",  # 运行时产生的各类文件的存储根目录
+
+    "start_epoch": 1,  # 训练时的起始epoch
+    "end_epoch": 20,  # 训练时的结束epoch
+
+    "best_dice": 0.60,  # 保存检查点的初始条件
+
+    "terminal_show_freq": 50,  # 终端打印统计信息的频率,以step为单位
+
+    # ————————————————————————————————————————————   测试相关参数   ———————————————————————————————————————————————————————
+
+    "crop_stride": [4, 4, 4]
+}
 
 
 
 
-def get_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--createData', action='store_true', default=False)
-    parser.add_argument('--resume', default=None, type=str, metavar='PATH',
-                        help='path to latest checkpoint (default: None)')
-    parser.add_argument('--pretrain', default=None, type=str, metavar='PATH',
-                        help='path to latest pretrain model (default: None)')
 
-    args = parser.parse_args()
 
-    return args
+
 
 
