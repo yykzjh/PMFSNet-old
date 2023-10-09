14c7001735c89d73e747ffbd6e1900c098eb3f83
unknown <1378453948@qq.com>
2023-10-09 16:22:08 +0800

    进一步修改可复现性配置

--- my-code/lib/utils/general.py
+++ my-code/lib/utils/general.py
@@ -13,19 +13,19 @@ import torch.backends.cudnn as cudnn
 
 
 def reproducibility(seed, deterministic, benchmark):
-    random.seed(seed)
+    random.seed(seed)  # 为python设置随机种子
     os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)  # 为numpy设置随机种子
+    torch.manual_seed(seed)  # 为CPU设置随机种子
+    torch.cuda.manual_seed(seed)  # 为当前GPU设置随机种子
+    torch.cuda.manual_seed_all(seed)  # 为所有GPU设置随机种子
 
     cudnn.deterministic = deterministic
     cudnn.benchmark = benchmark
-    g = torch.Generator()
-    g.manual_seed(seed)
+    torch.backends.cudnn.enabled = True
 
-    torch.use_deterministic_algorithms(True)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':16:8'
+    torch.use_deterministic_algorithms(False)
 
 
 
