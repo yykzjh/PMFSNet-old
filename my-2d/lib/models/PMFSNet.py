54948bd76c1a71b3b3ceab2710fed273ec53249f
unknown <1378453948@qq.com>
2023-11-17 01:04:39 +0800

    修改完成PMFSNet2D-basic模型

--- my-2d/lib/models/PMFSNet.py
+++ my-2d/lib/models/PMFSNet.py
@@ -102,11 +102,11 @@ class PMFSNet(nn.Module):
 #         super(PMFSNet, self).__init__()
 #
 #         kernel_sizes = [5, 3, 3]
-#         base_channels = [24, 24, 24]
-#         skip_channels = [12, 24, 24]
+#         base_channels = [24, 48, 64]
+#         skip_channels = [24, 48, 64]
 #         units = [5, 10, 10]
 #         growth_rates = [4, 8, 16]
-#         downsample_channels = [base_channels[i] + units[i] * growth_rates[i] for i in range(len(base_channels))]  # [44, 104, 184]
+#         downsample_channels = [base_channels[i] + units[i] * growth_rates[i] for i in range(len(base_channels))]  # [44, 128, 224]
 #
 #         self.down_convs = nn.ModuleList()
 #         for i in range(3):
@@ -126,9 +126,9 @@ class PMFSNet(nn.Module):
 #         # self.Global = global_module(
 #         #     in_channels=downsample_channels,
 #         #     max_pool_kernels=[4, 2, 1],
-#         #     ch=48,
-#         #     ch_k=48,
-#         #     ch_v=48,
+#         #     ch=64,
+#         #     ch_k=64,
+#         #     ch_v=64,
 #         #     br=3
 #         # )
 #
