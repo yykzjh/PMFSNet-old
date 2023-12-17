c4b93b44798303fa0743049da1ccd5782ff6e45f
unknown <1378453948@qq.com>
2023-12-18 01:04:46 +0800

    对测试集所有图像生成分割结果展示图

--- my-2d/test.py
+++ my-2d/test.py
@@ -26,8 +26,7 @@ import nni
 
 from lib import utils, dataloaders, models, losses, metrics, trainers
 import lib.transforms as my_transforms
-
-
+from lib.utils import *
 
 # 默认参数,这里的参数在后面添加到模型中，以params['dropout_rate']等替换原来的参数
 params = {
@@ -62,24 +61,21 @@ params = {
     "classes": 2,  # 模型最后输出的通道数,即类别总数
 
     "index_to_class_dict":  # 类别索引映射到类别名称的字典
-    {
-        0: "background",
-        1: "foreground"
-    },
+        {
+            0: "background",
+            1: "foreground"
+        },
 
     "pretrain": None,  # 是否需要加载预训练权重，如果需要则指定预训练权重文件路径
 }
 
 
-def segment_image(model, image_path, label_path):
+def segment_image(model, image, label):
     transform = my_transforms.Compose([
         my_transforms.Resize(params["resize_shape"]),
         my_transforms.ToTensor(),
         my_transforms.Normalize(mean=params["normalize_means"], std=params["normalize_stds"])
     ])
-    # 读取图像
-    image = cv2.imread(image_path, -1)
-    label = cv2.imread(label_path, -1)
     label[label == 255] = 1
     # 数据预处理和数据增强
     image, label = transform(image, label)
@@ -87,8 +83,6 @@ def segment_image(model, image_path, label_path):
     image = torch.unsqueeze(image, dim=0)
     # 转换数据格式
     label = label.to(dtype=torch.uint8)
-    # 放到cuda上
-
     # 预测分割
     pred = torch.squeeze(model(image.to(params["device"])), dim=0)
     segmented_image_np = torch.argmax(pred, dim=0).to(dtype=torch.uint8).cpu().numpy()
@@ -106,7 +100,8 @@ def segment_image(model, image_path, label_path):
     segmented_display_image = np.where(seg_image & label, white, segmented_display_image)
     segmented_display_image = np.where(seg_image & ~label, red, segmented_display_image)
     segmented_display_image = np.where(~seg_image & label, green, segmented_display_image)
-    return segmented_display_image
+    JI_score = cal_jaccard_index(seg_image, label)
+    return segmented_display_image, JI_score
 
 
 def generate_segment_result_images(model_names):
@@ -122,57 +117,51 @@ def generate_segment_result_images(model_names):
     print(params["device"])
     print("完成初始化配置")
 
-    # 遍历模型
-    for j, model_name in tqdm(enumerate(model_names)):
-        params["model_name"] = model_name
-        params["pretrain"] = os.path.join(r"./pretrain", model_name + ".pth")
-        # 初始化模型
-        model = models.get_model(params)
-        print("完成初始化模型:{}".format(params["model_name"]))
-        # 加载模型权重
-        pretrain_state_dict = torch.load(params["pretrain"], map_location=lambda storage, loc: storage.cuda(params["device"]))
-        model_state_dict = model.state_dict()
-        load_count = 0  # 成功加载参数计数
-        for param_name in model_state_dict.keys():
-            if (param_name in pretrain_state_dict) and (model_state_dict[param_name].size() == pretrain_state_dict[param_name].size()):
-                model_state_dict[param_name].copy_(pretrain_state_dict[param_name])
-                load_count += 1
-        model.load_state_dict(model_state_dict, strict=True)
-        print("{:.2f}%的模型参数成功加载预训练权重".format(100 * load_count / len(model_state_dict)))
-        # 遍历所有图
-        for i in tqdm(range(4)):
-            image_path = os.path.join(r"./images/ISIC2018_segment_result", str(i) + "0.jpg")
-            label_path = os.path.join(r"./images/ISIC2018_segment_result", str(i) + "1.png")
-            segmented_display_image = segment_image(model, image_path, label_path)
-            segmented_display_image = segmented_display_image[:, :, ::-1]
-            cv2.imwrite(os.path.join(r"./images/ISIC2018_segment_result", str(i) + str(j+2) + ".jpg"), segmented_display_image)
+    dataset_root_dir = r"./datasets/ISIC2018_train_test/test"
+    images_dir = os.path.join(dataset_root_dir, "images")
+    labels_dir = os.path.join(dataset_root_dir, "annotations")
+    cnt = 0
+    # 遍历每张图像
+    for image_name in tqdm(os.listdir(images_dir)):
+        filename, ext = os.path.splitext(image_name)
+        image_path = os.path.join(images_dir, image_name)
+        label_path = os.path.join(labels_dir, filename + "_segmentation.png")
+        # 读取图像
+        image = cv2.imread(image_path, -1)
+        label = cv2.imread(label_path, -1)
+        max_JI_score = 0
+        max_model_name = None
+        segment_result_images_list = []
 
-
+        # 遍历所有模型
+        for model_name in model_names:
+            params["model_name"] = model_name
+            params["pretrain"] = os.path.join(r"./pretrain", model_name + ".pth")
+            # 初始化模型
+            model = models.get_model(params)
+            # 加载模型权重
+            pretrain_state_dict = torch.load(params["pretrain"], map_location=lambda storage, loc: storage.cuda(params["device"]))
+            model_state_dict = model.state_dict()
+            for param_name in model_state_dict.keys():
+                if (param_name in pretrain_state_dict) and (model_state_dict[param_name].size() == pretrain_state_dict[param_name].size()):
+                    model_state_dict[param_name].copy_(pretrain_state_dict[param_name])
+            model.load_state_dict(model_state_dict, strict=True)
+            # 分割
+            seg_result_image, JI_score = segment_image(model, image.copy(), label.copy())
+            segment_result_images_list.append(seg_result_image)
+            if JI_score > max_JI_score:
+                max_JI_score = JI_score
+                max_model_name = model_name
+        if max_model_name == "PMFSNet":
+            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+            cv2.imwrite(os.path.join(r"./images/ISIC2018_segment_result", "{:04d}".format(cnt) + "_0.jpg"), image)
+            label = cv2.resize(label, (224, 224), interpolation=cv2.INTER_NEAREST)
+            cv2.imwrite(os.path.join(r"./images/ISIC2018_segment_result", "{:04d}".format(cnt) + "_1.jpg"), label)
+            for j, segment_result_image in enumerate(segment_result_images_list):
+                segment_result_image = segment_result_image[:, :, ::-1]
+                cv2.imwrite(os.path.join(r"./images/ISIC2018_segment_result", "{:04d}".format(cnt) + "_" + str(j + 2) + ".jpg"), segment_result_image)
+            cnt += 1
 
 
 if __name__ == '__main__':
     generate_segment_result_images(["UNet", "AttU_Net", "CANet", "BCDUNet", "CENet", "CPFNet", "CKDNet", "PMFSNet"])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
